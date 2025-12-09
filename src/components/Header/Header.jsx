13f4cd7bcf257334c2777c2b0e034fed07178255
Your Name <your_email@example.com>
2025-12-09 14:36:56 +0530

    Initial Commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+  return (
+    <div className='header'>
+        <div className="header-contents">
+            <h2>Order your favourite food here</h2>
+            <p>Where every bite is crafted with passion and care, every aroma invites you into a world of comfort and warmth, and every visit becomes a memory worth savoring. From the first welcoming smile to the last lingering taste, we create an experience that blends flavor, hospitality, and connection—turning each meal into a celebration you’ll want to relive again and again.</p>
+            <button>View Menu</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
