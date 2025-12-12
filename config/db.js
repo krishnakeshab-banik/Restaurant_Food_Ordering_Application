055dbb543af25ea01c0c57e029f637ab16f461fa
Your Name <your_email@example.com>
2025-12-13 01:13:24 +0530

    Restructured project into frontend + backend folders

--- config/db.js
+++
@@ -1,5 +0,0 @@
-import mongoose from "mongoose";
-
-export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://food_order:FoodOrder123@cluster0.dg3ppx3.mongodb.net/food-order').then(() => console.log("Connected to MongoDB")).catch((err) => console.error(err));
-}
